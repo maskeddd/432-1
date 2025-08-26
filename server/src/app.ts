2e457e4fdb4148898ce0c7fc81aca967f8322f60
masked <37945842+maskeddd@users.noreply.github.com>
2025-08-26 18:47:19 +1000

    fix: temp cors fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,17 @@
+import cors from "cors"
 import express from "express"
 import { errorHandler } from "./middlewares/errorHandler.middleware.ts"
 import clipperRoutes from "./routes/clipper.routes.ts"
 
 const app = express()
+
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+)
 
 app.use("/", clipperRoutes)
 
